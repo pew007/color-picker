3132c1767255ee4bc9a8eed99028466767d8ceb1
Pengyu Wang <bird0407@gmail.com>
2015-09-30 21:03:36 -0700

    init commit

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var red: UITextField!
+    @IBOutlet weak var green: UITextField!
+    @IBOutlet weak var blue: UITextField!
+
+    @IBAction func changeColor(sender: UIButton) {
+        print("Red: \(red!.text)")
+        print("Red: \(green!.text)")
+        print("Red: \(blue!.text)")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
